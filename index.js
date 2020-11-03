98bc9131ff581184ae759878fc3c7dfb99d6db6a
Rob Osborn <rosborn@webstacks.com>
2020-11-03 09:41:26 -0800

    error tracking

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const axios = require('axios');
-// const path = require('path')
 require('dotenv').config()
 const { handleProd } = require('./utils/products')
 const app = express()
 app.use(bodyParser.json())
+
 const PORT = process.env.PORT || 3000
 
 const stripe = require('stripe')(process.env.STRIPE_PROD_SK);
@@ -47,22 +47,42 @@ app.post('/cancel_subscription', async (req, res) => {
 app.post('/successful_payment', async (req, res) => {
     const payload = req.body.data.object
     const customerId = payload.customer
-    const customer = await stripe.customers.retrieve(customerId);
+    let customer;
+
+    try {
+        customer = await stripe.customers.retrieve(customerId);
+    } catch (e) {
+        console.error("ERROR: Could not retrieve customer by Id");
+    }
+
     const email = customer.email
     const invoice = payload.invoice
 
     let date = new Date()
     date = date.setUTCHours(0, 0, 0, 0)
 
-    const invoiceData = await stripe.invoices.retrieve(invoice);
-    const product = invoiceData.lines.data[0].plan.product
-    const prodInfo = handleProd(product)
+    let invoiceData;
 
     try {
-        const userRes = await axios.get(`https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HAPI_KEY}`)
-        const userData = userRes.data
-        const userId = userData.vid
+        invoiceData = await stripe.invoices.retrieve(invoice);
+    } catch (e) {
+        console.error("ERROR: Could not retrieve invoice data.");
+    }
+    const product = invoiceData.lines.data[0].plan.product
+    const prodInfo = handleProd(product)
+    console.log("PROD INFO", prodInfo);
 
+    let userRes;
+    try {
+        userRes = await axios.get(`https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HAPI_KEY}`)
+    } catch (e) {
+        console.error("ERROR: Could not find Hubspot User");
+    }
+
+    const userData = userRes.data
+    const userId = userData.vid
+
+    try {
         if (prodInfo.prod === "Authorify") {
             await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "dppp_trial_last_payment_date", value: date }] }, { "Content-Type": "application/json" })
         } else if (prodInfo.prod === "RMA") {
@@ -71,7 +91,7 @@ app.post('/successful_payment', async (req, res) => {
             await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "dfy_last_payment_date", value: date }] }, { "Content-Type": "application/json" })
         }
     } catch (e) {
-        console.log("ERROR", e);
+        console.log("ERROR: Could not update User Contact");
     }
 
     res.status(200).send()
