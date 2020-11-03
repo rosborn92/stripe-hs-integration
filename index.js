a773ecd7ff35d4d7e7d4f983f9464ec2747d5dc6
Rob Osborn <rosborn@webstacks.com>
2020-11-02 16:21:04 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const axios = require('axios');
+// const path = require('path')
+require('dotenv').config()
+const { handleProd } = require('./utils/products')
+const app = express()
+app.use(bodyParser.json())
+const PORT = process.env.PORT || 3000
+
+const stripe = require('stripe')(process.env.STRIPE_TEST_SK);
+
+app.post('/cancel_subscription', async (req, res) => {
+    const payload = req.body.data.object
+    const product = payload?.items?.data[0].price.produc
+    const customerId = payload?.customer
+    const prodInfo = handleProd(product)
+    const customer = await stripe.customers.retrieve(customerId);
+    const email = customer.email
+
+    let date = new Date()
+    date = date.setUTCHours(0, 0, 0, 0)
+
+    try {
+        const userRes = await axios.get(`https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HAPI_KEY}`)
+        const userData = userRes.data
+        const userId = userData.vid
+
+        if (prodInfo.prod === "Authorify") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "authorify_dpp_cancel_date", value: date }] }, { "Content-Type": "application/json" })
+        } else if (prodInfo.prod === "RMA") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "rm_cancel_date", value: date }] }, { "Content-Type": "application/json" })
+        } else if (prodInfo.prod === "DFY") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "dfy_canceled_date", value: date }] }, { "Content-Type": "application/json" })
+        }
+
+    } catch (e) {
+        console.log("ERROR", e);
+    }
+    res.status(200).send()
+})
+
+app.post('/successful_payment', async (req, res) => {
+    const payload = req.body.data.object
+    const customerId = payload.customer
+    const customer = await stripe.customers.retrieve(customerId);
+    const email = customer.email
+    const invoice = payload.invoice
+
+    let date = new Date()
+    date = date.setUTCHours(0, 0, 0, 0)
+
+    const invoiceData = await stripe.invoices.retrieve(invoice);
+    const product = invoiceData.lines.data[0].plan.product
+    const prodInfo = handleProd(product)
+
+    try {
+        const userRes = await axios.get(`https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HAPI_KEY}`)
+        const userData = userRes.data
+        const userId = userData.vid
+
+        if (prodInfo.prod === "Authorify") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "dppp_trial_last_payment_date", value: date }] }, { "Content-Type": "application/json" })
+        } else if (prodInfo.prod === "RMA") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "rm_last_payment_date", value: date }] }, { "Content-Type": "application/json" })
+        } else if (prodInfo.prod === "DFY") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "dfy_last_payment_date", value: date }] }, { "Content-Type": "application/json" })
+        }
+    } catch (e) {
+        console.log("ERROR", e);
+    }
+
+    res.status(200).send()
+})
+
+app.post('/failed_payment', async (req, res) => {
+    const payload = req.body.data.object
+    const customerId = payload.customer
+    const customer = await stripe.customers.retrieve(customerId);
+    const email = customer.email
+    const invoice = payload.invoice
+
+    let date = new Date()
+    date = date.setUTCHours(0, 0, 0, 0)
+
+    const invoiceData = await stripe.invoices.retrieve(invoice);
+    const product = invoiceData.lines.data[0].plan.product
+    const prodInfo = handleProd(product)
+
+    try {
+        const userRes = await axios.get(`https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HAPI_KEY}`)
+        const userData = userRes.data
+        const userId = userData.vid
+
+        if (prodInfo.prod === "Authorify") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "dppptrial_hold_date", value: date }] }, { "Content-Type": "application/json" })
+        } else if (prodInfo.prod === "RMA") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "rm_hold_date", value: date }] }, { "Content-Type": "application/json" })
+        } else if (prodInfo.prod === "DFY") {
+            await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "dfy_hold_date", value: date }] }, { "Content-Type": "application/json" })
+        }
+    } catch (e) {
+        console.log("ERROR", e);
+    }
+
+    res.status(200).send()
+})
+
+app.post('/expiring_card', async (req, res) => {
+    const email = req.body.data.object.owner.email
+
+    try {
+        const userRes = await axios.get(`https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HAPI_KEY}`)
+        const userData = userRes.data
+        const userId = userData.vid
+
+
+        await axios.post(`https://api.hubapi.com/contacts/v1/contact/vid/${userId}/profile?hapikey=${process.env.HAPI_KEY}`, { properties: [{ property: "expired_credit_card", value: "Yes" }] }, { "Content-Type": "application/json" })
+    } catch (e) {
+        console.log("ERROR", e);
+    }
+
+    res.status(200).send()
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
---
+++ utils/products.js
@@ -0,0 +1,88 @@
+const handleProd = (productId) => {
+    switch (productId) {
+        // case "prod_IIH7RrPpa2OkUg": // PROD
+        case "prod_IIJDXvgHTLbldl": // DEV
+            return {
+                name: "Authorify Digital Monthly Memberships",
+                prod: "Authorify"
+            }
+        // case "prod_IIH7dwjQeJjrPo": // PROD
+        case "prod_IIIm7cRiUJ6yF9": // DEV
+            return {
+                name: "Authorify Digital Annual Memberships",
+                prod: "Authorify"
+            }
+        // case "prod_IIH7MrtHWAHGiS": // PROD
+        case "prod_III8H78OhIsleA": // DEV
+            return {
+                name: "Referral Marketing Annual",
+                prod: "RMA"
+            }
+        // case "prod_IIH7f3l6dNhqqO": // PROD
+        case "prod_IIIG84Qg04URPS": // DEV
+            return {
+                name: "Referral Marketing Monthly",
+                prod: "RMA"
+            }
+        // case "prod_IIH7BllxUwrBwA": // PROD
+        case "prod_IIILrLjtnw1YEI": // DEV
+            return {
+                name: "Books on Demand",
+                prod: "DFY"
+            }
+        // case "prod_IIH7szIpvaqH6R": // PROD
+        case "prod_IIIWGp1UHfBiyR": // DEV
+            return {
+                name: "DFY Seller Guides",
+                prod: "DFY"
+            }
+        // case "prod_IIH7FzfC86pwZZ": // PROD
+        case "prod_IIIb6LCYM30ZRm": // DEV
+            return {
+                name: "DFY FSBO Guides",
+                prod: "DFY"
+            }
+        // case "prod_IIH6CslJrLJ6sQ": // PROD
+        case "prod_IIIflzf71sJJNs": // DEV
+            return {
+                name: "DFY Expired Guides",
+                prod: "DFY"
+            }
+        // case "prod_IHR6opYjiz60iV": // PROD
+        case "prod_IHR77yhDVvCdpW": // DEV
+            return {
+                name: "Authorify Digital Membership Monthly",
+                prod: "Authorify"
+            }
+        // case "57844425": // PROD
+        case "57844425": // DEV
+            return {
+                name: "Digital Trial",
+                prod: "Digital Trial"
+            }
+        // case "prod_I56zgOlVWSndkq": // PROD
+        case "prod_I56zgOlVWSndkq": // DEV
+            return {
+                name: "Authorify Digital Membership Annual $997",
+                prod: "Authorify"
+            }
+        // case "prod_I53duMFVHVWu8K": // PROD
+        case "prod_IH467IIZnDsQgM": // DEV
+            return {
+                name: "Authorify Digital+Plus Membership $97",
+                prod: "Authorify"
+            }
+        // case "prod_FMcEiIFh8DOZD2": // PROD
+        case "prod_FMcFUIKJl5e4xq": // DEV
+            return {
+                name: "SmartAgents Labs - Gold",
+                prod: "SmartAgents Labs - Gold"
+            }
+        default:
+            return {
+                error: "product not found in directory."
+            }
+    }
+}
+
+module.exports = { handleProd }
